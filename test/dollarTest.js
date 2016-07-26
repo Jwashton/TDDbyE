7600537484fa5bee6e27fec29c65ce2717da0478
William Ashton <William@AshtonFam.org>
2016-07-26 12:35:50 -0400

    Add first test

--- test/dollarTest.js
+++ test/dollarTest.js
@@ -3,3 +3,11 @@ import test from 'ava';
 test('Canary test', t => {
   t.pass();
 });
+
+test('Multiplication', t => {
+  const five = createDollar(5);
+
+  five.times(2);
+
+  t.is(10, five.amount);
+});
