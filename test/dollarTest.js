2fb587145207fb3a3562aa667fb84731857b21fb
William Ashton <William@AshtonFam.org>
2016-08-23 12:42:18 -0400

    Remove Dollar and Franc classes

--- test/dollarTest.js
+++ test/dollarTest.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { mint, createMoney, createFranc } from '../lib/dollar';
+import { mint } from '../lib/dollar';
 
 test('Canary test', t => {
   t.pass();
@@ -12,23 +12,10 @@ test('Multiplication', t => {
   t.true(mint.dollars(15).equals(five.times(3)));
 });
 
-test('FrancMultiplication', t => {
-  const five = mint.francs(5);
-
-  t.true(mint.francs(10).equals(five.times(2)));
-  t.true(mint.francs(15).equals(five.times(3)));
-});
-
 test('Equality', t => {
   t.true(mint.dollars(5).equals(mint.dollars(5)));
   t.false(mint.dollars(5).equals(mint.dollars(6)));
-  t.true(mint.francs(5).equals(mint.francs(5)));
-  t.false(mint.francs(5).equals(mint.francs(6)));
   t.false(mint.francs(5).equals(mint.dollars(5)));
-});
-
-test('DifferentClassEquality', t => {
-  t.true(createMoney(10, 'CHF').equals(createFranc(10, 'CHF')));
 });
 
 test('Currency', t => {
